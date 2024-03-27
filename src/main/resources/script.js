14214d8f99e6853f9b45b0f173a85a08905f19c5
RaulC-Pirvan-TUIASI <Raul-Costin.Pirvan@student.tuiasi.ro>
2024-03-27 13:17:59 +0200

    [27032024-1317] Finisaje

--- src/main/resources/script.js
+++ src/main/resources/script.js
@@ -2,16 +2,18 @@ function previewImage(event) {
     var image = document.getElementById('preview');
     var imagePreview = document.getElementById('imagePreview');
     var dropAreaText = document.getElementById('dropAreaText');
+    var orText = document.getElementById('orText');
     var chooseFileButton = document.querySelector('.custom-file-upload'); // Select the button by class
 
     // Hide the drop area text and choose file button
     // Test
     dropAreaText.style.display = 'none';
+    orText.style.display = 'none';
     chooseFileButton.style.display = 'none';
 
     // Set the image source to the uploaded file
     var reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
         image.src = e.target.result;
     }
     reader.readAsDataURL(event.target.files[0]);
@@ -19,6 +21,32 @@ function previewImage(event) {
     // Show the image preview
     imagePreview.classList.remove('hidden');
 
+    dropArea.style.height = '300px';
+
+    dropArea.classList.add('uploaded');
+
     // Adjust the height of the drop area (optional)
-    document.getElementById('dropArea').style.height = '500px'; // Adjust the height as needed
+    document.getElementById('dropArea').style.height = '300px'; // Adjust the height as needed
+
+    // Update current year
+    document.getElementById("currentYear").innerText = new Date().getFullYear();
 }
+
+function handleDrop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var files = event.dataTransfer.files;
+    if (files.length > 0) {
+        previewImage({target: {files: files}});
+    }
+}
+
+function handleDragOver(event) {
+    event.preventDefault();
+    event.stopPropagation();
+}
+
+var dropArea = document.getElementById('dropArea');
+dropArea.addEventListener('dragover', handleDragOver);
+dropArea.addEventListener('drop', handleDrop);
