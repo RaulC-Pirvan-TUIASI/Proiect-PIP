50a412610e273c1a1df108fe73f112fe8a3b5d24
Marius13 <marius-vasile.maftei@student.tuiasi.ro>
2024-05-24 15:15:18 +0300

    [24.05.2024] [+] curatat de erori MainTest [+] javaDOC la MainTest [+] package-info.java pt MainTest

--- src/main/java/future/MainTEST.java
+++ src/main/java/future/MainTEST.java
@@ -4,12 +4,37 @@ import ocr.JavaReadTextFromImage;
 
 import java.io.File;
 
-public class MainTEST {
+/**
+ * The MainTEST class serves as an entry point
+ * for testing the OCR (Optical Character Recognition)
+ *
+ * functionality provided by the {@link JavaReadTextFromImage} class.
+ */
+//final
+public final class MainTEST {
+    /**
+     * Private constructor to prevent instantiation of the utility class.
+     */
+    private MainTEST() {
+        // Private constructor to prevent instantiation
+        throw new IllegalStateException("Utility class");
+    }
 
-    public static void main(String[] args) throws Exception
-    {
-        String calea = "H:\\Other computers\\My Laptop\\Javra\\PIPpr\\Proiect-PIP\\pozici\\grilaCapitale.png";
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws Exception the exception
+     */
+    //final
+    public static void main(final String[] args) throws Exception {
+        // Path to the image file to be processed
+        String calea = "pozici/grilaCapitale.png";
+
+        //Create an instance of JavaReadTextFromImage class for OCR processing
         JavaReadTextFromImage ocrProcessor = new JavaReadTextFromImage();
+
+        // Perform OCR on the specified image file and print the result
         System.out.println(ocrProcessor.performocr(new File(calea)));
 
     }
